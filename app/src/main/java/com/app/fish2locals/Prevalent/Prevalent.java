612e84f3f533241d9f5aed149a57b97bc0f0e2b5
Chrislee Nieve <chrislee.delossantos@gmail.com>
2022-03-28 20:23:32 +0800

    first commit

---
+++ app/src/main/java/com/app/fish2locals/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.app.fish2locals.Prevalent;
+
+import com.app.fish2locals.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
